86bed81f65be689d2e08dace356fd6dfd050f2f6
嬗衍 <shanyanwcx@qq.com>
2024-10-26 22:03:36 +0800

    支持ipv6访问

--- app.js
+++ app.js
@@ -95,6 +95,6 @@ app.use((req, res, next) => {
   res.status(404).send("404 - 页面不存在。");
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, '::', () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
